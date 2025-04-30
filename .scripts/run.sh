634798ce70a3fa85a9b62ffb3f7730182590fbbf
Raphael apeh <132306548+RaphaelApeh@users.noreply.github.com>
2025-04-30 14:03:45 +0100

    Update run.sh

--- .scripts/run.sh
+++ .scripts/run.sh
@@ -8,6 +8,6 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py collectstatic --on-input
+python manage.py collectstatic --no-input
 
 gunicorn beckings.wsgi:application --bind 0.0.0.0:8080
