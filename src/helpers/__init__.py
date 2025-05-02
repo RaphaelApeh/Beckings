c4b47df33d61585b9f7acbd709359f3f414cd045
raphael apeh <raphaelapeh37@gmail.com>
2025-05-02 21:40:44 +0100

    working on products view

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,21 +1,6 @@
-from django.http import HttpRequest
 
 from .serializers import UserSerializer
 
-
 __all__ = [
-    "UserSerializer",
-    "parse_request"
+    "UserSerializer"
 ]
-
-def parse_request[T](args: list[T], raise_exception=False) -> HttpRequest | None:
-    _request = None
-
-    for bit in args:
-        if isinstance(bit, HttpRequest):
-            _request = HttpRequest
-            break
-    if raise_exception and _request is None:
-        raise TypeError
-    return _request
-
--- src/products/views.py
+++ src/products/views.py
@@ -3,7 +3,7 @@ from django.views.generic import (
     View,
     DetailView
     )
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
@@ -17,13 +17,13 @@ class ProductListView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
 
         queryset = self.queryset
-        
+
+        context = {"queryset": queryset}
+
         if hasattr(request, "htmx") and request.htmx:
             queryset = queryset.filter()
 
-        context = {"queryset": queryset}
         template_name = self.template_name
-
         return render(request, template_name, context)
     
 
@@ -34,4 +34,12 @@ class ProductDetailView(DetailView):
     template_name = "products/product-detail.html"
     query_pk_and_slug = True
 
+    def get_object(self, queryset = None):
+
+        #assert isinstance(queryset, QuerySet)
+        model = queryset.model if queryset is not None else None or self.get_queryset().model
+        kwargs = {"pk": self.kwargs["pk"], "product_slug": self.kwargs["slug"]}
+        obj = get_object_or_404(model, **kwargs)
+
+        return obj
 
