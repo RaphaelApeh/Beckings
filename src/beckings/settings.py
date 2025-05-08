81fafc039e9271d5537d87f32847b8d1d26779a6
raphael apeh <raphaelapeh37@gmail.com>
2025-05-08 22:07:44 +0100

    added products template

--- src/beckings/settings.py
+++ src/beckings/settings.py
@@ -85,6 +85,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                
+                "helpers.context_processors.default_price",
             ],
             "builtins": [
                 "django.templatetags.static",
---
+++ src/helpers/context_processors.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+from django.conf import settings
+from django.http import HttpRequest
+
+
+def default_price(request: HttpRequest) -> dict[str, Any]:
+
+    return {
+        "DEFAULT_PRICE_CURRENCY": getattr(settings, "DEFAULT_PRICE_CURRENCY", "$")
+    }
+
