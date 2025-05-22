6915e2bb6c3fc8052279aa9b00230d1b6bc7a962
raphael apeh <raphaelapeh37@gmail.com>
2025-05-22 13:27:26 +0100

    added login view test

--- src/tests/test_views/test_login_views.py
+++ src/tests/test_views/test_login_views.py
@@ -1,6 +1,32 @@
 import pytest
+from pytest_django import asserts as astx
+
+from django.urls import reverse
 
 
 @pytest.mark.django_db
 class TestLoginView:
-    pass
+    
+
+    def test_valid_login(self, client) -> None:
+        data = {
+            "login": "test_user",
+            "password": "password"
+        }
+        
+        response = client.post(reverse("login"), data)
+
+        assert response.status_code == 302
+
+        astx.assertRedirects(response, reverse("products"))
+
+
+    def test_invalid_login(self, client) -> None:
+
+        data = {}
+        data["login"] = "invaliduser"
+        response = client.post(reverse("login"), data)
+
+        assert "This field is required." in response.text
+
+
