ca509fa0ce289385a2e776ba4c5d9cf592cf58f8
raphael apeh <raphaelapeh37@gmail.com>
2025-09-28 21:24:43 +0100

    added feedback view

--- src/feedbacks/views.py
+++ src/feedbacks/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from typing import Dict
 
-# Create your views here.
+from django.views import generic
+from django.contrib import messages
+from django.template.response import TemplateResponse
+from django.utils.translation import gettext_lazy as _
+
+from .models import FeedBack
+from .forms import FeedBackForm
+
+class FeedBackCreateView(generic.CreateView):
+
+    form_class = FeedBackForm
+    model = FeedBack
+    template_name = "feedbacks/create_feedback.html"
+    success_template_name = "feedbacks/success.html"
+
+
+    def get_form_kwargs(self) -> Dict:
+        user = (
+            user if (user := self.request.user) and not user.is_anonymous else None
+        )
+        return {**super().get_form_kwargs(), "user": user}
+    
+    def form_valid(self, form):
+        request = self.request
+        user, obj = form.save()
+        self.object = obj
+        messages.success(request, _("Feed back successfully sent."))
+        # request.user might be AnonymousUser
+        return TemplateResponse(
+            request,
+            self.success_template_name,
+            self.get_context_data(object=obj, user_obj=user)
+        )
